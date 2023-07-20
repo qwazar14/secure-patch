1c02aba1e2420c52a3e219739171f7bf19353f39
qwazar14 <zenez89@gmail.com>
2023-07-20 11:18:26 +0300

    Updated to version 1.0.5: Added settings for all security patch options to the WordPress admin panel.

--- secure-patch.php
+++ secure-patch.php
@@ -3,7 +3,7 @@
 Plugin Name: Secure Patch Plugin
 Description: A plugin to increase the security of your WordPress site.
 Author: Maksym "Qwazar" Mezhyrytskyi
-Version: 1.0.4
+Version: 1.0.5
 Author URI: https://github.com/qwazar14/
 Plugin URI: https://github.com/qwazar14/secure-patch
 */
@@ -14,16 +14,12 @@ class SecurePatchPlugin
 
     public function __construct()
     {
-        remove_action('wp_head', 'wp_generator');
-        add_filter('the_generator', [$this, 'remove_version_info']);
-        add_filter('style_loader_src', [$this, 'remove_version_from_style_js'], 9999);
-        add_filter('script_loader_src', [$this, 'remove_version_from_style_js'], 9999);
-        add_filter('password_enforce_strong_password', [$this, 'enforce_strong_password']);
-        add_filter('rest_authentication_errors', [$this, 'disable_rest_api']);
-
         $this->options = get_option('secure_patch_plugin', [
             'disable_xml_rpc' => 0,
-            'disable_file_edit' => 0
+            'disable_file_edit' => 0,
+            'remove_wp_version' => 0,
+            'enforce_strong_passwords' => 0,
+            'disable_rest_api' => 0
         ]);
 
         if ($this->options['disable_xml_rpc']) {
@@ -34,13 +30,23 @@ class SecurePatchPlugin
             define('DISALLOW_FILE_EDIT', true);
         }
 
+        if ($this->options['remove_wp_version']) {
+            remove_action('wp_head', 'wp_generator');
+            add_filter('the_generator', '__return_false');
+            add_filter('style_loader_src', [$this, 'remove_version_from_style_js'], 9999);
+            add_filter('script_loader_src', [$this, 'remove_version_from_style_js'], 9999);
+        }
+
+        if ($this->options['enforce_strong_passwords']) {
+            add_filter('password_enforce_strong_password', [$this, 'enforce_strong_password']);
+        }
+
+        if ($this->options['disable_rest_api']) {
+            add_filter('rest_authentication_errors', [$this, 'disable_rest_api']);
+        }
+
         register_setting('secure_patch_plugin', 'secure_patch_plugin', [$this, 'validate_options']);
         add_action('admin_menu', [$this, 'admin_menu']);
-    }
-
-    public function remove_version_info()
-    {
-        return '';
     }
 
     public function remove_version_from_style_js($src)
@@ -106,6 +112,31 @@ class SecurePatchPlugin
                             <label for="disable_file_edit">Disable File Edit</label>
                         </td>
                     </tr>
+                    <tr valign="top">
+                        <th scope="row">Remove WordPress Version</th>
+                        <td>
+                            <input type="checkbox" id="remove_wp_version" name="secure_patch_plugin[remove_wp_version]"
+                                   value="1" <?php checked(1, $this->options['remove_wp_version'], true); ?> />
+                            <label for="remove_wp_version">Remove WordPress Version</label>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Enforce Strong Passwords</th>
+                        <td>
+                            <input type="checkbox" id="enforce_strong_passwords"
+                                   name="secure_patch_plugin[enforce_strong_passwords]"
+                                   value="1" <?php checked(1, $this->options['enforce_strong_passwords'], true); ?> />
+                            <label for="enforce_strong_passwords">Enforce Strong Passwords</label>
+                        </td>
+                    </tr>
+                    <tr valign="top">
+                        <th scope="row">Disable REST API</th>
+                        <td>
+                            <input type="checkbox" id="disable_rest_api" name="secure_patch_plugin[disable_rest_api]"
+                                   value="1" <?php checked(1, $this->options['disable_rest_api'], true); ?> />
+                            <label for="disable_rest_api">Disable REST API</label>
+                        </td>
+                    </tr>
                 </table>
                 <?php submit_button(); ?>
             </form>
@@ -117,6 +148,9 @@ class SecurePatchPlugin
     {
         $newinput['disable_xml_rpc'] = isset($input['disable_xml_rpc']) ? 1 : 0;
         $newinput['disable_file_edit'] = isset($input['disable_file_edit']) ? 1 : 0;
+        $newinput['remove_wp_version'] = isset($input['remove_wp_version']) ? 1 : 0;
+        $newinput['enforce_strong_passwords'] = isset($input['enforce_strong_passwords']) ? 1 : 0;
+        $newinput['disable_rest_api'] = isset($input['disable_rest_api']) ? 1 : 0;
         return $newinput;
     }
 }
